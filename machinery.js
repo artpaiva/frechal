c9b8d647fd4f27722f65a4c91606b1af65b70239
arturromero <artur.romero@vix.com>
2019-11-05 16:11:03 -0300

    Frechal Starter

---
+++ machinery.js
@@ -0,0 +1,13 @@
+
+
+window.onscroll = function (){ 
+    var cornice  = document.getElementById('cornice');
+    console.log(document.body.scrollTop);
+    if (document.body.scrollTop > 0 ) {
+        cornice.classList.add("scroll");
+        console.log(cornice.classList);
+    } 
+    else {
+        cornice.classList.remove("scroll");
+    }
+};

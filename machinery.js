3b952313d0f8752c46194f189e740b91e7d1d769
arturromero <artur.romero@vix.com>
2019-11-07 17:07:25 -0300

    Theming

--- machinery.js
+++ machinery.js
@@ -1,17 +1,33 @@
 
+var scrollClass = 'scrolle';
 
 window.onscroll = function (){ 
     var cornice  = document.getElementById('cornice');
     console.log(document.body.scrollTop);
     if (document.body.scrollTop > 0 ) {
-        cornice.classList.add("scroll");
+        cornice.classList.add(scrollClass);
         console.log(cornice.classList);
     } 
     else {
-        cornice.classList.remove("scroll");
+        cornice.classList.remove(scrollClass);
     }
 };
 
 function turnTorsoPanel(ele) {
     ele.classList.toggle("active");
+}
+
+function changeTheme() {
+  var check = document.getElementById('check-theme');
+  if (check.checked == true){
+    console.log('Dark Theme');
+    scrollClass = 'scroll';
+    cornice.classList.add(scrollClass);
+    cornice.classList.remove('scrolle');
+  } else {
+    console.log('Light Theme');
+    scrollClass = 'scrolle';
+    cornice.classList.add(scrollClass);
+    cornice.classList.remove('scroll');
+  }
 }
